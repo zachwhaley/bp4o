f71f8f4c580d0e79ca5bc5fdbec6e43171897838
Zachary Whaley <zachbwhaley@gmail.com>
2016-04-13 10:38:37 -0500

    Switch to XDG directory setup
    This puts all data and config files for BP4O in XDG style directories.
    e.g. ~/.local/share/bp4o/ and ~/.config/bp4o/

--- install.sh
+++ install.sh
@@ -2,20 +2,21 @@
 set -e
 
 prefix="/usr/local"
-[ -n $1 ] && prefix=$1
+[ -n "$1" ] && prefix=$1
+data="${XDG_DATA_HOME:=$HOME/.local/share}/bp4o"
 
 rm -rf $prefix/bin/p4-*
-rm -rf $prefix/share/bp4o.sh
+rm -rf $data/init.sh
 
-mkdir -p $prefix/bin $prefix/share
+mkdir -p $prefix/bin $data
 for b in bin/p4-*; do
     install -v $b "$prefix/$b"
 done
-install -v -m 0444 bp4o.sh "$prefix/share/bp4o.sh"
+install -v -m 0444 init.sh "$data/init.sh"
 
 echo "
 BP4O Installed!
 Now add the following to your shell's rc file to setup BP4O on login
 
-[ -f $prefix/share/bp4o.sh ] && source $prefix/share/bp4o.sh
+[ -f $data/init.sh ] && source $data/init.sh
 "
