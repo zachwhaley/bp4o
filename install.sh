982145433582612d2b7a822137997d602f090257
Zachary Whaley <zachbwhaley@gmail.com>
2016-04-14 04:10:26 +0000

    install: Change data var name to share
    because I'm fickle

--- install.sh
+++ install.sh
@@ -2,22 +2,22 @@
 set -e
 
 prefix="/usr/local"
-[ -n "$1" ] && prefix=$1
-data="/usr/local/share/bp4o"
-[ $(whoami) != "root" ] && data="${XDG_DATA_HOME:=$HOME/.local/share}/bp4o"
+[ -n "$1" ] && prefix="$1"
+share="/usr/local/share/bp4o"
+[ $(whoami) != "root" ] && share="${XDG_DATA_HOME:=$HOME/.local/share}/bp4o"
 
 rm -rf $prefix/bin/p4-*
-rm -rf $data/init.*
+rm -rf $share/init.*
 
-mkdir -p $prefix/bin $data
+mkdir -p $prefix/bin $share
 for b in bin/p4-*; do
     install -v $b "$prefix/$b"
 done
 for i in init.*; do
-    install -v -m 0444 $i "$data/$i"
+    install -v -m 0444 $i "$share/$i"
 done
 
-init="${data/#$HOME/~}/init.${SHELL##*/}"
+init="${share/#$HOME/~}/init.${SHELL##*/}"
 echo "
 BP4O Installed!
 Now add the following to your shell's rc file to setup BP4O on login
