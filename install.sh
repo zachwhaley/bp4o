36f957f06f314ce9f4106beb50ab0cb33923ff94
Zachary Whaley <zachbwhaley@gmail.com>
2016-04-14 02:27:26 +0000

    Only support bash for the moment
    I tried to support both bash and zsh in one shell function, but that was getting difficult.
    So instead I'm going to separate the bash and zsh p4 functions.  For now, here is the bash
    function.

--- install.sh
+++ install.sh
@@ -6,17 +6,17 @@ prefix="/usr/local"
 data="${XDG_DATA_HOME:=$HOME/.local/share}/bp4o"
 
 rm -rf $prefix/bin/p4-*
-rm -rf $data/init.sh
+rm -rf $data/init.bash
 
 mkdir -p $prefix/bin $data
 for b in bin/p4-*; do
     install -v $b "$prefix/$b"
 done
-install -v -m 0444 init.sh "$data/init.sh"
+install -v -m 0444 init.bash "$data/init.bash"
 
 echo "
 BP4O Installed!
 Now add the following to your shell's rc file to setup BP4O on login
 
-[ -f $data/init.sh ] && source $data/init.sh
+[ -f $data/init.bash ] && source $data/init.bash
 "
