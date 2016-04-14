1488d048ae1e7efd40a95e59040b577eed72eb51
Zachary Whaley <zachbwhaley@gmail.com>
2016-04-14 03:39:10 +0000

    Add zsh support
    init.zsh is a zsh specific file to source in zsh for the p4 function.
    It looks very similar to init.bash, but it has its differences.
    
    1. 'a=( a b ); echo $a' prints 'a' in bash and 'a b' in zsh.
    2. bash 'type -P' == zsh 'whence -p'
    3. differences in parameter substitution

---
+++ init.zsh
@@ -0,0 +1,18 @@
+p4() {
+    typeset -a cmd
+    local cmd=( $@ )
+
+    # Search for and apply aliases from the bp4o aliases file
+    local config="${XDG_CONFIG_HOME:=$HOME/.config}/bp4o"
+    local alias=$(sed -n -r "s/$cmd[1]\s*=\s*(.+)/\1/p" "$config/aliases" 2>/dev/null)
+    if [ -n  "$alias" ]; then
+        cmd=( ${=cmd/#$cmd[1]/$alias} )
+    fi
+
+    p4bin=$(whence -p p4)
+    if type p4-$cmd[1] &>/dev/null; then
+        p4-$cmd[1] $p4bin $cmd
+    else
+        $p4bin $cmd
+    fi
+}
--- install.sh
+++ install.sh
@@ -6,17 +6,19 @@ prefix="/usr/local"
 data="${XDG_DATA_HOME:=$HOME/.local/share}/bp4o"
 
 rm -rf $prefix/bin/p4-*
-rm -rf $data/init.bash
+rm -rf $data/init.*
 
 mkdir -p $prefix/bin $data
 for b in bin/p4-*; do
     install -v $b "$prefix/$b"
 done
-install -v -m 0444 init.bash "$data/init.bash"
+for i in init.*; do
+    install -v -m 0444 $i "$data/$i"
+done
 
 echo "
 BP4O Installed!
 Now add the following to your shell's rc file to setup BP4O on login
 
-[ -f $data/init.bash ] && source $data/init.bash
+[ -f $data/init.${SHELL##*/} ] && source $data/init.${SHELL##*/}
 "
