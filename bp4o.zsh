7d2aa9906fb3e603c7440e018643a17d19d38700
Zachary Whaley <zwhaley@cisco.com>
2016-10-14 16:19:58 +0000

    Run p4 -V once during login to avoid slowness
    Calling `p4 -V` for every p4 call can cause slowness waiting for p4 to connect to the server, or whatever it does.
    
    Since the p4 version should not change during a login session, we can run `p4 -V` once during
    login, capture the result, and use that for every subsequent p4 call.

--- bp4o.zsh
+++ bp4o.zsh
@@ -1,12 +1,12 @@
+export P4VERSION=$(command p4 -V | awk -F/ '/Rev. P4/ {print substr($3,1,4)}')
 p4() {
     typeset -a args
     local args=( $@ )
     local cmd=$args[1]
 
-    local p4verion=$(p4 -V | awk -F/ '/Rev. P4/ {print substr($3,1,4)}')
     local p4aliases=$HOME/.p4aliases
     local bp4oaliases=${XDG_CONFIG_HOME:-$HOME/.config}/bp4o/aliases
-    if [ $p4version -ge 2016 ] && [ -f $p4aliases ]; then
+    if [ $P4VERSION -ge 2016 ] && [ -f $p4aliases ]; then
         # Search for and apply aliases from Perforce
         local p4cmd=$(perl -n -e "print if \$_ =~ s/^$args[1]\s*.*=\s*(\w+).*/\1/" $p4aliases)
         if [ -n "$p4cmd" ]; then
