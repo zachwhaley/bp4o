d8c5b0264ed51a33a2ffd8ec30de6325c6be03f5
Zach Whaley <zachbwhaley@gmail.com>
2016-12-02 11:46:02 -0600

    Appropriately set Fish variable scopes

--- bp4o.fish
+++ bp4o.fish
@@ -1,7 +1,7 @@
 set -x P4VERSION (command p4 -V | awk -F/ '/Rev. P4/ {print $3}')
 function p4
-    set args $argv
-    set cmd $args[1]
+    set -l args $argv
+    set -l cmd $args[1]
 
     set -l p4aliases $HOME/.p4aliases
     if set -q P4ALIASES
@@ -13,24 +13,25 @@ function p4
         set bp4oaliases $BP4OALIASES
     end
 
-    set p4v (string split . $P4VERSION)
-    if test $p4v[1] -ge 2016; and test -f $p4aliases
+    set -l p4year (string split . $P4VERSION)
+
+    if test $p4year[1] -ge 2016; and test -f $p4aliases
         # Search for and apply aliases from Perforce
-        set p4cmd (perl -n -e "print if \$_ =~ s/^$args[1]\b.*=\s*(\w+).*/\1/" $p4aliases)
-        if test -n "$p4cmd"
+        set -l p4cmd (perl -n -e "print if \$_ =~ s/^$args[1]\b.*=\s*(\w+).*/\1/" $p4aliases)
+        if test -n $p4cmd
             set cmd $p4cmd
         end
     else if test -f $bp4oaliases
         # Search for and apply aliases from BP4O
-        set alias (perl -n -e "print if \$_ =~ s/$args[1]\s*=\s*(.+)/\1/" $bp4oaliases)
-        if test -n "$alias"
+        set -l alias (perl -n -e "print if \$_ =~ s/$args[1]\s*=\s*(.+)/\1/" $bp4oaliases)
+        if test -n $alias
             set args (string replace "$args[1]" "$alias" "$args")
             set args (string split " " $args)
             set cmd $args[1]
         end
     end
 
-    set p4bin (type -P p4)
+    set -l p4bin (type -P p4)
     if command which p4-$cmd > /dev/null ^&1
         eval p4-$cmd $p4bin $args
     else
